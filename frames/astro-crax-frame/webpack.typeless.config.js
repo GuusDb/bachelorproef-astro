b6e9c4da1d51c3ae053b16b250119f5b8ba95d0e
GuusDeBruyn <de.bruyn.guus@gmail.com>
2023-03-12 11:28:55 +0100

    configs

---
+++ frames/astro-crax-frame/webpack.typeless.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const federationConfig = require('./federation.config.json');
+const { dependencies } = require('./package.json');
+
+const port = 3000;
+
+module.exports = {
+  output: {
+    publicPath: `http://localhost:${port}/`,
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    port,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: 'javascript/auto',
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { modules: true },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      filename: 'remoteEntry.js',
+      ...federationConfig,
+      shared: {
+        react: {
+          singleton: true,
+          requiredVersion: dependencies.react,
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: dependencies['react-dom'],
+        },
+        'react-router-dom': {
+          singleton: true,
+          requiredVersion: dependencies['react-router-dom'],
+        },
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
