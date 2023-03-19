84f8f7b6d9c8ab5d4e8df97756f67fd87a4c514b
GuusDeBruyn <de.bruyn.guus@gmail.com>
2023-03-19 11:25:00 +0100

    help

--- apps/crax-test-app/src/components/button.tsx
+++ apps/crax-test-app/src/components/button.tsx
@@ -1,6 +1,12 @@
 import { useState } from 'react';
 
 export function Button() {
+	/* const {
+        isLoading,
+        data: immos,
+        isError,
+      } = useGet<produ[]>('Immo', '/immo');*/
+
 	const [clickCount, setClickCount] = useState<number>(0);
 	const handleClick = () => {
 		setClickCount(clickCount + 1);
---
+++ frames/react-crax-frame/.wp_federation/frame/dts/types/api-types.d.ts
@@ -0,0 +1,7 @@
+export declare type product = {
+    id: number;
+    title: string;
+    price: number;
+    category: string;
+    description: string;
+};
