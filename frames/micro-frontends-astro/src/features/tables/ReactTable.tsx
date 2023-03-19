fd88441de0c2d88422500634608fe46d6cf2cda5
GuusDeBruyn <de.bruyn.guus@gmail.com>
2023-03-19 12:45:33 +0100

    com

--- frames/micro-frontends-astro/src/features/tables/ReactTable.tsx
+++ frames/micro-frontends-astro/src/features/tables/ReactTable.tsx
@@ -7,5 +7,28 @@ export function ReactTable() {
   useEffect(() => {
     console.log($products);
   }, [$products]);
-  return <div></div>;
+  return (
+    <table>
+      <tr>
+        <th scope='col'>id</th>
+        <th scope='col'>title</th>
+        <th scope='col'>price</th>
+        <th scope='col'>category</th>
+        <th scope='col'>description</th>
+        <th scope='col'>image</th>
+      </tr>
+      {$products.map((product) => {
+        return (
+          <tr>
+            <td scope='row'>{product.id}</td>
+            <td scope='row'>{product.title}</td>
+            <td scope='row'>{product.price}</td>
+            <td scope='row'>{product.category}</td>
+            <td scope='row'>{product.description}</td>
+            <td scope='row'>{product.image}</td>
+          </tr>
+        );
+      })}
+    </table>
+  );
 }
